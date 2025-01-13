7f7e11191f8d13835eeae8f1493549079cd89ff2
szymon <szymonmajewski.777@gmail.com>
2025-01-13 13:27:39 +0100

    Automatic generation of image prompts

--- src/video_app.py
+++ src/video_app.py
@@ -3,26 +3,38 @@ from dropbox_uploader import upload_to_dropbox
 from ai_generator import generate_text, generate_image
 from tts import generate_audio
 from video_maker import generate_video
+from datetime import datetime
 
-text_prompt = "Write a short, motivational text for an instagram post about the benefits of doing pull-ups."
-read_text_prompt = "Write a short, motivational text about the benefits of doing pull-ups that will be used as a voiceover in an instagram video. Write only the text that will be read."
-image_prompts = [
-    "An european and naturally looking, young woman with a gentle smile performing a pull-up exercise at the gym.",
-    "A black and naturally looking young man performing a pull-up exercise at the gym.",
-    "A black, young, naturally looking man with a gentle smile performing a pull-up exercise at the outdoor gym.",
-    "An asian female teenager performing a pull-up exercise at the gym.",
-    "An european and naturally looking young man performing a pull-up exercise at the outdoor gym.",
-]
-base_path = "pullup_benefits"
+num_images = 6
+text_prompt = "Write a short, motivational text for an instagram post about the benefits of running."
+read_text_prompt = "Write a short, motivational text about the benefits of running that will be used as a voiceover in an instagram video. Write only the text that will be read."
+images_generation_prompts = (
+    f"Generate {num_images} diverse and detailed image prompts for a gym-related Instagram video. "
+    f"The prompts should feature people running on a treadmill or running outdoors, and the descriptions should specify diversity "
+    f"in gender, ethnicity, and setting (e.g. gym, outdoor). Example format: '<Description of person> running <location>'."
+    f"Separate the prompts with new line characters."
+)
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+base_path = f"jogging_{timestamp}"
 audio_path = f"{base_path}.mp3"
 video_path = f"{base_path}.mp4"
 
 instagram_caption = generate_text(text_prompt)
-with open('caption_for_final_video.txt', 'w') as file:
-    file.write(instagram_caption)
+print(instagram_caption)
 
 read_text = generate_text(read_text_prompt).split("#")[0] + " Check out the description for more info on the topic!"
+print(read_text)
+
 caption_audio = generate_audio(read_text, audio_path)
+
+raw_image_prompts = generate_text(images_generation_prompts).split("\n")
+image_prompts = []
+for i, prompt in enumerate(raw_image_prompts):
+    if i % 2 == 0:
+        image_prompt = prompt.split(" ", 1)[-1]
+        image_prompts.append(image_prompt)
+print(image_prompts)
+assert len(image_prompts) == num_images
 images_paths = []
 for i, image_prompt in enumerate(image_prompts):
     image_path = f"{base_path}_{i}.png"
@@ -31,5 +43,5 @@ for i, image_prompt in enumerate(image_prompts):
 
 generate_video(images_paths, audio_path, video_path)
 
-image_dropbox_link = upload_to_dropbox(image_path, f"/gymrat/{image_path}")
-upload_image_to_instagram(image_dropbox_link, instagram_caption)
+# image_dropbox_link = upload_to_dropbox(image_path, f"/gymrat/{image_path}")
+# upload_image_to_instagram(image_dropbox_link, instagram_caption)
