6f0e7ead834b59e2b5a60fcccc079b9a01b5535a
szymon <szymonmajewski.777@gmail.com>
2025-02-06 18:52:39 +0100

    Added transitions between clips in the video

--- src/video_app.py
+++ src/video_app.py
@@ -4,9 +4,9 @@ from video_maker import generate_video
 from datetime import datetime
 import os
 
-num_images = 7
-topic = "benefits of weighted squats"
+topic = "high protein breakfast ideas"
 topic_with_underscores = topic.replace(" ", "_")
+num_images = 7
 
 text_prompt = f"Write a short, motivational text for an instagram post about the {topic}."
 read_text_prompt = f"Write a short, motivational text about the {topic} that will be used as a voiceover in an instagram video. Write only the text that will be read."
@@ -28,34 +28,34 @@ print(f"Path to media: {base_path}")
 instagram_caption = generate_text(text_prompt)
 with open("reflinks.txt", 'r') as reflinks_file:
     reflinks = reflinks_file.read()
-    instagram_caption = f'{instagram_caption}\n{reflinks}'
+    instagram_caption = f'{instagram_caption}\n\n{reflinks}'
 with open(f"generations/{base_path}/caption.txt", 'w') as caption_file:
     caption_file.write(instagram_caption)
 
-# read_text = generate_text(read_text_prompt).split("#")[0] + " Check out the description for more info on the topic!"
-# with open(f"generations/{base_path}/read_text.txt", 'w') as read_text_file:
-#     read_text_file.write(read_text)
-# 
-# caption_audio = generate_audio(read_text, audio_path)
-# 
-# raw_image_prompts = generate_text(images_generation_prompts).split("\n")
-# image_prompts = []
-# for i, prompt in enumerate(raw_image_prompts):
-#     if i % 2 == 0:
-#         image_prompt = prompt.split(" ", 1)[-1]
-#         image_prompts.append(image_prompt)
-# with open(f"generations/{base_path}/image_prompts.txt", 'w') as image_prompts_file:
-#     for prompt in image_prompts:
-#         image_prompts_file.write(f"{prompt}\n")
-# 
-# assert len(image_prompts) == num_images
-# images_paths = []
-# for i, image_prompt in enumerate(image_prompts):
-#     image_path = f"generations/{base_path}/{base_path}_{i}.png"
-#     images_paths.append(image_path)
-#     try:
-#         generate_image(image_prompt, image_path)
-#     except Exception as error:
-#         print("An error occured while generating image:", error)
-# 
-# generate_video(images_paths, audio_path, video_path)
+read_text = generate_text(read_text_prompt).split("#")[0] + " Check out the description for more info on the topic!"
+with open(f"generations/{base_path}/read_text.txt", 'w') as read_text_file:
+    read_text_file.write(read_text)
+
+caption_audio = generate_audio(read_text, audio_path)
+
+raw_image_prompts = generate_text(images_generation_prompts).split("\n")
+image_prompts = []
+for i, prompt in enumerate(raw_image_prompts):
+    if i % 2 == 0:
+        image_prompt = prompt.split(" ", 1)[-1]
+        image_prompts.append(image_prompt)
+with open(f"generations/{base_path}/image_prompts.txt", 'w') as image_prompts_file:
+    for prompt in image_prompts:
+        image_prompts_file.write(f"{prompt}\n")
+
+assert len(image_prompts) == num_images
+images_paths = []
+for i, image_prompt in enumerate(image_prompts):
+    image_path = f"generations/{base_path}/{base_path}_{i}.png"
+    images_paths.append(image_path)
+    try:
+        generate_image(image_prompt, image_path)
+    except Exception as error:
+        print("An error occured while generating image:", error)
+
+generate_video(images_paths, audio_path, video_path)
--- src/video_maker.py
+++ src/video_maker.py
@@ -1,17 +1,19 @@
-from moviepy import ImageClip, AudioFileClip, concatenate_videoclips
-from pydub import AudioSegment
+from moviepy.editor import ImageClip, AudioFileClip, concatenate_videoclips
 
 
-def generate_video(images_paths, audio_path, output_path):
-    clips = []
-    audio = AudioSegment.from_file(audio_path)
-    duration_per_image = len(audio) / len(images_paths) / 1000
-
-    for image_path in images_paths:
-        clip = ImageClip(image_path)
-        clip.duration = duration_per_image
-        clips.append(clip)
-    video = concatenate_videoclips(clips, method="compose")
+def generate_video(images_paths, audio_path, output_path, transition_duration=1):
     audio = AudioFileClip(audio_path)
-    video.audio = audio
-    video.write_videofile(output_path, fps=24)
+    duration_per_image = audio.duration / len(images_paths)
+
+    video_clips = []
+    for idx, image_path in enumerate(images_paths):
+        clip = ImageClip(image_path).set_duration(duration_per_image + transition_duration)
+
+        if idx != 0:
+            clip = clip.crossfadein(transition_duration)
+
+        video_clips.append(clip)
+
+    final_video = concatenate_videoclips(video_clips, method="compose", padding=-transition_duration)
+    final_video = final_video.set_audio(audio)
+    final_video.write_videofile(output_path, fps=24)
